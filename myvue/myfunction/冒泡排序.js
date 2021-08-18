626b6c04288d3598e89696fe29f0eef7b6a5c044
guotianliang <guotianliang@aihujing.com>
2021-08-18 19:14:36 +0800

    fix aa

--- myvue/myfunction/冒泡排序.js
+++ myvue/myfunction/冒泡排序.js
@@ -17,3 +17,32 @@ function bubbleSort(arr){
   var arr = [3,6,5,1]
   bubbleSort(arr);
   console.log(arr)
+
+  function a(){
+    this.b=function(){
+      console.log(123)
+    };
+    this.c=2
+  }
+  function b(){
+    a.call(this);
+  }
+  let c=new b();
+  c.b();
+console.log(c)
+ 231
+function bubuleSort(arr){
+  if(!arr?.length){
+    throw new Error("数组为0")
+  }
+  for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length-1-i;j++){
+      if(arr[j]>arr[j+1]){
+        let a =arr[j];
+        arr[j] =arr[j+1];
+        arr[j+1] =a
+      }
+    }
+  }
+  return arr
+}
---
+++ myvue/劫持.js
@@ -0,0 +1,18 @@
+function aa(){
+
+  console.log(123,456)
+}
+function bb(){
+  console.log(7777777777777)
+}
+Function.prototype.before=function(cb){
+  cb();
+  return this;
+}
+// Function.prototype.before=function(cb){
+//   cb();
+//   return this;
+// }
+aa.before(bb);
+aa();
+aa.after(bb)
