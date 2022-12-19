50031eadc7360310c7e7f67df3ef924e7a86fb19
guotianliang <guotianliang.bj.@acewill.cn>
2022-12-19 15:21:41 +0800

    🔧 framework:测试

--- myvue/继承的几种方式.js
+++ myvue/继承的几种方式.js
@@ -1,24 +1,25 @@
 // 基于原型链的继承
-function superFn(){
+function SuperFn(){
 }
-superFn.prototype.name="我是prototype上的";
-superFn.prototype.say=function (){
-    console.log("haha")
-}
-function suberFn(){}
-suberFn.prototype = new superFn();
-suberFn.prototype.constract=suberFn;
+SuperFn.prototype.name="我是prototype上的";
+SuperFn.prototype.say=function (){
+    console.log("haha");
+};
+function SuberFn(){}
+SuberFn.prototype = new SuperFn();
+SuberFn.prototype.constract=SuberFn;
 
-let mysuber = new suberFn();
+let mysuber = new SuberFn();
+
 console.log(mysuber.name);
 mysuber.say();
 
 // 缺点：当某一个实例修改原型链上某一个属性时，如果实例类型是引用类型，那么其它实例的属性也会被修改。
 
 // 基于call的继承
-function suberFn(){
-    superFn.call(this,'参数')
-}
+// function suberFn(){
+//     superFn.call(this,'参数');
+// }
 // 缺点：继承不了原型链上的属性和方法
 
 // 基于组合继承
@@ -27,26 +28,27 @@ function createObj(o) {
     function F() { }
     F.prototype = o;
     return new F();
-  }
-  //继承原型属性 即原型式继承
-  function create(parent, child) {
+}
+//继承原型属性 即原型式继承
+function create(parent, child) {
     var f = createObj(parent.prototype);//获取原型对象
-    child.prototype = f
-    child.prototype.constructor = child;//增强对象原型，即保持原有constructor指向
-  }
 
-  function Parent(name) {
+    child.prototype = f;
+    child.prototype.constructor = child;//增强对象原型，即保持原有constructor指向
+}
+
+function Parent(name) {
     this.name = name;
     this.arr = ['brother', 'sister', 'parents'];
-  }
-  Parent.prototype.run = function () {
+}
+Parent.prototype.run = function () {
     return this.name;
-  };
-  function Child(name, age) {
+};
+function Child(name, age) {
     // 示例属性
     Parent.call(this, name);
     this.age = age;
-  }
-  // 原型属性继承寄生于该方法中
-  create(Parent.prototype,Child);
-  var child1 = new Child('trigkit4', 21);
+}
+// 原型属性继承寄生于该方法中
+create(Parent.prototype,Child);
+var child1 = new Child('trigkit4', 21);
